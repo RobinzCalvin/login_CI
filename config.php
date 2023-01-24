844633b1aae53982fc20694d7222f998a02d7908
Robinz Calvin <robinzcalvin@gmail.com>
2023-01-24 18:53:08 -0500

    config update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /** General Configuration */
 
 // Base Site URL`s (e.g., http://example.com/)
-define('BASE_URL', 'http://localhost/codeigniter-simple-login/');
+define('BASE_URL', 'http://localhost/login/');
 
 // The hostname of your database server
 define('DB_HOSTNAME', 'localhost');
